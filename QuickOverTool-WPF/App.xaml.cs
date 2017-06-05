18ca4955ad81e9ca67a08a9c78e9b7583250e035
xyx0826 <xyx0826@hotmail.com>
2017-06-04 21:42:38 -0400

    添加项目文件。

---
+++ QuickOverTool-WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuickOverTool_WPF
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

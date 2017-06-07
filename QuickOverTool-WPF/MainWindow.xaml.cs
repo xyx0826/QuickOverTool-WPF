0f7d8cebce6427b28869142b69863f2c145c4957
xyx0826 <xyx0826@hotmail.com>
2017-06-07 09:45:36 -0400

    检查单

--- QuickOverTool-WPF/MainWindow.xaml.cs
+++ QuickOverTool-WPF/MainWindow.xaml.cs
@@ -24,35 +24,58 @@ namespace QuickOverTool_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly BackgroundWorker worker = new BackgroundWorker();
-
+        // 全局变量：二进制文件路径
+        private string sharedPath;
+        private string buildInfoPath;
+        private string overToolExecutable;
+        private string cascLibDll;
+        // 全局变量：.build.info 读取
+        private StreamReader buildInfoStream;
+        // 主窗体
         public MainWindow()
         {
             InitializeComponent();
-            checkOverToolValidity();
-        }
 
+            // 启动检查守望先锋 & OverTool 有效性
+            checkOverToolValidity();
+            if (!String.IsNullOrEmpty(textBoxPath.Text))
+            {
+                checkOverwatchValidity(textBoxPath.Text);
+            }
+
+            updateChecklist();
+        }
+        // 检查守望先锋
         public bool checkOverwatchValidity(string path)
         {
-            if (File.Exists(Path.Combine(path, ".build.info")))
+            buildInfoPath = Path.Combine(path, ".build.info");
+            if (File.Exists(buildInfoPath))
             {
-                string overwatchExecutablePath = Path.Combine(path, "Overwatch.exe");
+                buildInfoPath = Path.Combine(path, ".build.info");
+                labelValidity.Content = "守望先锋目录有效";
+                textBoxPath.BorderBrush = new SolidColorBrush(Colors.Blue);
+                labelValidity.Foreground = new SolidColorBrush(Colors.Green);
                 return true;
             }
             else
             {
+                buildInfoPath = null;
+                labelValidity.Content = "守望先锋目录无效";
+                textBoxPath.BorderBrush = new SolidColorBrush(Colors.Red);
+                labelValidity.Foreground = new SolidColorBrush(Colors.Red);
                 return false;
             }
         }
-
+        // 检查 OverTool
         public bool checkOverToolValidity()
         {
-            string overToolLocation = Path.GetDirectoryName
-                (Assembly.GetEntryAssembly().CodeBase);
-            overToolLocation = Path.Combine(overToolLocation.Substring(6),
-                "OverTool.exe");
-            if (File.Exists(overToolLocation))
+            sharedPath = Path.GetDirectoryName
+                (Assembly.GetEntryAssembly().CodeBase).Substring(6);
+            overToolExecutable = Path.Combine
+                (sharedPath, "OverTool.exe");
+            if (File.Exists(overToolExecutable))
             {
+                cascLibDll = Path.Combine(sharedPath, "CascLib.dll");
                 labelValidity.Content = "检测到了 OverTool";
                 labelValidity.Foreground = new SolidColorBrush(Colors.Green);
                 return true;
@@ -64,12 +87,83 @@ namespace QuickOverTool_WPF
                 return false;
             }
         }
+        // 检查单更新
+        public void updateChecklist()
+        {
+            // Overwatch Version
 
+            if (checkOverToolValidity())
+            {
+                labelOverToolExecutable.Foreground = new SolidColorBrush(Colors.Green);
+                labelOverToolExecutable.Content = "有效";
+            }
+            else
+            {
+                labelOverToolExecutable.Foreground = new SolidColorBrush(Colors.Red);
+                labelOverToolExecutable.Content = "无效";
+            }
+
+            if (File.Exists(Path.Combine(sharedPath, "OWLib.dll")) &&
+                File.Exists(Path.Combine(sharedPath, "CascLib.dll")) &&
+                File.Exists(Path.Combine(sharedPath, "ow.keys")) &&
+                File.Exists(Path.Combine(sharedPath, "ow.events")))
+            {
+                labelOverToolIntegrity.Foreground = new SolidColorBrush(Colors.Green);
+                labelOverToolIntegrity.Content = "完整";
+            }
+            else
+            {
+                labelOverToolIntegrity.Foreground = new SolidColorBrush(Colors.Red);
+                labelOverToolIntegrity.Content = "不完整";
+            }
+
+            labelCascLibVersion.Content =
+                FileVersionInfo.GetVersionInfo(Path.Combine(sharedPath, "CascLib.dll")).FileVersion;
+
+            buildInfoReader();
+        }
+
+        public void buildInfoReader()
+        {
+            string buildInfoRead;
+            if (string.IsNullOrEmpty(buildInfoPath)) return;
+            else
+            {
+                buildInfoStream = new StreamReader(buildInfoPath);
+                {
+                    try
+                    {
+                        while (buildInfoStream.Peek() >= 0)
+                        {
+                            buildInfoRead = buildInfoStream.ReadLine();
+
+                            while (!buildInfoRead.StartsWith("us"))
+                            {
+                                buildInfoRead = buildInfoStream.ReadLine();
+                                if (buildInfoRead.StartsWith("us")) break;
+                            }
+                            
+                            var buildInfoParsed = buildInfoRead.Split('|');
+                            labelOverwatchVersion.Content = buildInfoParsed[11];
+                            labelOverwatchBranch.Content = buildInfoParsed[6];
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        labelOverwatchVersion.Content = "无法读取";
+                        labelOverwatchBranch.Content = "无法读取";
+                    }
+                    
+                }
+            }
+        }
+        // 日志输出增行
         public void addLog(string content)
         {
             textBoxOutput.AppendText("\n" + content);
         }
-
+        // 检测模式选择
         private string whichRadioButton()
         {
             if (radioButtonListGeneralCosmetics.IsChecked == true) return "g";
@@ -90,21 +184,14 @@ namespace QuickOverTool_WPF
             else if (radioButtonExtractLootbox.IsChecked == true) return "L";
             else return "";
         }
-
+        // 走你
         private void buttonPath_Click(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
             DialogResult folderBrowserResult = folderBrowser.ShowDialog();
-            if (checkOverwatchValidity(textBoxPath.Text) == true)
-            {
-                labelValidity.Content = "守望先锋目录有效";
-                labelValidity.Foreground = new SolidColorBrush(Colors.Green);
-            }
-            else
-            {
-                labelValidity.Content = "守望先锋目录无效";
-                labelValidity.Foreground = new SolidColorBrush(Colors.Red);
-            }
+            textBoxPath.Text = folderBrowser.SelectedPath;
+            checkOverwatchValidity(textBoxPath.Text);
+            buildInfoReader();
         }
 
         private void buttonOutputPath_Click(object sender, RoutedEventArgs e)
@@ -119,25 +206,15 @@ namespace QuickOverTool_WPF
         private void buttonStart_Click(object sender, RoutedEventArgs e)
         {
             // 判断：是否选择了守望先锋路径
-            if (String.IsNullOrEmpty(textBoxPath.Text) ||
-                labelValidity.Content.ToString() == "守望先锋目录无效")
-            {
-                textBoxPath.BorderBrush = new SolidColorBrush(Colors.Red);
-                return;
-            }
+            if (labelValidity.Content.ToString() == "守望先锋目录无效") return;
             else if (checkOverwatchValidity(textBoxPath.Text) == true)
-            {
-                textBoxPath.BorderBrush = new SolidColorBrush(Colors.Blue);
                 textBoxPath.IsEnabled = false;
-            }
-            else return;
-
             // 构建命令行
             string cmdLine;
             // 命令行：选定语言
             cmdLine = " --language=" + comboBoxLanguage.SelectedItem.
                 ToString().Substring(38, 4);
-            // 命令行：禁用项
+            // 命令行：复选框
             if (checkBoxSkipAnimation.IsChecked == true) cmdLine = cmdLine + " -A";
             if (checkBoxSkipGUI.IsChecked == true) cmdLine = cmdLine + "-I";
             if (checkBoxSkipKeys.IsChecked == true) cmdLine = cmdLine + "-n";
@@ -145,7 +222,6 @@ namespace QuickOverTool_WPF
             if (checkBoxSkipRef.IsChecked == true) cmdLine = cmdLine + "-R";
             if (checkBoxSkipSound.IsChecked == true) cmdLine = cmdLine + "-S";
             if (checkBoxSkipTexture.IsChecked == true) cmdLine = cmdLine + "-T";
-            // 命令行：杂项
             if (checkBoxExpert.IsChecked == true) cmdLine = cmdLine + "--ex";
             if (checkBoxCollision.IsChecked == true) cmdLine = cmdLine + "-C";
             // 命令行：守望先锋路径

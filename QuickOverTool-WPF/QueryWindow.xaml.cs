912f6c4522676fd60b961be7ac8a9b0e1a3abd49
xyx0826 <xyx0826@hotmail.com>
2017-11-11 15:13:27 -0500

    Beautify query editor code

--- QuickOverTool-WPF/QueryWindow.xaml.cs
+++ QuickOverTool-WPF/QueryWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
 
 namespace QuickOverTool_WPF
 {
@@ -8,7 +9,6 @@ namespace QuickOverTool_WPF
     /// </summary>
     public partial class QueryWindow : Window
     {
-
         public QueryWindow()
         {
             InitializeComponent();
@@ -19,6 +19,7 @@ namespace QuickOverTool_WPF
             e.Cancel = true;
             windowQuery.Hide();
         }
+
         // Method for passing the query back to MainWindow
         public string GetQueries()
         {
@@ -39,35 +40,12 @@ namespace QuickOverTool_WPF
         {
             windowQuery.Hide();
         }
-        // Will replace with more beautiful code later
-        private void buttonSkin_Click(object sender, RoutedEventArgs e)
+        // Trace to the button that fired the event
+        private void buttonTypes_Click(object sender, RoutedEventArgs e)
         {
-            textBoxQuery.Text += "|skin=";
-        }
-
-        private void buttonIcon_Click(object sender, RoutedEventArgs e)
-        {
-            textBoxQuery.Text += "|icon=";
-        }
-
-        private void buttonSpray_Click(object sender, RoutedEventArgs e)
-        {
-            textBoxQuery.Text += "|spray=";
-        }
-
-        private void buttonEmote_Click(object sender, RoutedEventArgs e)
-        {
-            textBoxQuery.Text += "|emote=";
-        }
-
-        private void buttonPose_Click(object sender, RoutedEventArgs e)
-        {
-            textBoxQuery.Text += "|victorypose=";
-        }
-
-        private void buttonIntro_Click(object sender, RoutedEventArgs e)
-        {
-            textBoxQuery.Text += "|highlightintro=";
+            System.Windows.Controls.Button button = 
+                (System.Windows.Controls.Button)sender;
+            textBoxQuery.Text += ("|" + button.Content.ToString().ToLower() + "=");
         }
 
         private void butonCommon_Click(object sender, RoutedEventArgs e)
@@ -83,11 +61,6 @@ namespace QuickOverTool_WPF
         private void buttonLegendary_Click(object sender, RoutedEventArgs e)
         {
             textBoxQuery.Text += "(rarity=legedary)";
-        }
-
-        private void buttonHelp_Click(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }

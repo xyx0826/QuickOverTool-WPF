a4fb9c4d9721362f925cd50730d9db85bd18c2c0
xyx0826 <xyx0826@hotmail.com>
2017-11-14 11:06:54 -0500

    XAML isn't the place for storing data

--- QuickOverTool-WPF/AboutWindow.xaml.cs
+++ QuickOverTool-WPF/AboutWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Data;
 
 namespace QuickOverTool_WPF
 {
@@ -7,10 +8,29 @@ namespace QuickOverTool_WPF
     /// </summary>
     public partial class AboutWindow : Window
     {
+        public string Text
+        {
+            get
+            {
+                return "QuickDataTool is made by xyx0826.\n" +
+                    "This project can be found at:\n\n" +
+                    "https://github.com/xyx0826/QuickOverTool-WPF/tree/datatool. \n\n" +
+                    "Thanks dynaomi, zingballyhoo, SombraOW and Js41637 for making OverTool toolchain, the ultimate toolset for extracting Overwatch game assets.\n\n" +
+                    "QuickDataTool is my experimental project. It is my first WPF program.\n\n" +
+                    "The GUI for OverTool (Overwatch pre-1.14) is called QuickOverTool, it can be found under the master branch in the GitHub repo.\n\n" +
+                    "If you need a GUI for OverTool-ing pre-1.14 game, please consider checking out Yernemm's OverTool GUI. They did an awesome job at writing a handy interface that even comes with file conversion.\n\n" +
+                    "You can find Yernemm's GUI at:\n\n" +
+                    "https://yernemm.xyz/projects/OverToolGUI.";
+
+            }
+            set { }
+        }
+
         public AboutWindow()
         {
             InitializeComponent();
         }
+        
         // Todo: add data through binding, instead of XAML
     }
 }
